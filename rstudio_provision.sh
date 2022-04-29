2bd98797d0bb59bb6f5edd80972a8c960715d97a
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-04-29 12:30:06 -0400

    Adding confirmation flags to apt-get commands. Now starts server correctly

--- rstudio_provision.sh
+++ rstudio_provision.sh
@@ -8,7 +8,7 @@ set -x
 apt-get update
 
 # install two helper packages we need
-apt install --no-install-recommends software-properties-common dirmngr git
+apt install -y --no-install-recommends software-properties-common dirmngr git
 
 # add the signing key (by Michael Rutter) for these repos
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
@@ -19,12 +19,12 @@ wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sud
 add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $(lsb_release -cs)-cran40/"
 
 # Finally, install R
-apt-get install --no-install-recommends r-base
+apt-get install -y --no-install-recommends r-base
 
 # Install rstudio
 apt-get install gdebi-core
 wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-2022.02.2-485-amd64.deb
-gdebi rstudio-server-2022.02.2-485-amd64.deb
+gdebi -n rstudio-server-2022.02.2-485-amd64.deb
 
 # clone this repo so we have any helper scripts (e.g. add_users.sh) available if we SSH onto the VM
 # for creation of users and other management.
